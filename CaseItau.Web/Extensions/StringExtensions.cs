9b14e3bbaa6fcbf0fdb6cdb7cc18e1e0e2d30f68
Rafael Simoes <rafaelsimoes133@outlook.com>
2024-06-26 00:04:50 -0300

    fix telas

---
+++ CaseItau.Web/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace CaseItau.Web.Extensions;
+
+public static class StringExtensions
+{
+    public static string FormatCnpj(this string cnpj)
+    {
+        if (string.IsNullOrWhiteSpace(cnpj) || cnpj.Length != 14)
+            return cnpj;
+
+        return $"{cnpj.Substring(0, 2)}.{cnpj.Substring(2, 3)}.{cnpj.Substring(5, 3)}/{cnpj.Substring(8, 4)}-{cnpj.Substring(12, 2)}";
+    }
+
+    public static string FormatPatrimony(this decimal? patrimony)
+    {
+        if (patrimony == null)
+        {
+            return "Não informado";
+        }
+        else
+        {
+            return string.Format(System.Globalization.CultureInfo.GetCultureInfo("pt-BR"), "R$ {0:N2}", patrimony.Value);
+        }
+    }
+}
